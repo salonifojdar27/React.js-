381d7521af066e50cfbc3b1b62f0525e4930e0f3
salonifojdar27 <salonifojdar27@gmail.com>
2025-07-10 13:28:52 +0530

    added assignment-2

---
+++ Assignment 2 (list)/src/App.jsx
@@ -0,0 +1,12 @@
+import MobileOs from "./components/MobileOs"
+
+function App() {
+
+  return (
+    <div className="app">
+      <MobileOs />
+    </div>
+  )
+}
+
+export default App
---
+++ Assignment 2 (list)/src/components/MobileOs.jsx
@@ -0,0 +1,24 @@
+
+function MobileOs() {
+    return (
+        <div>
+            <h1>Mobile OPerating System</h1>
+            <ul>
+                <li>Android</li>
+                <li>Blackberry</li>
+                <li>iPhone</li>
+                <li>Windows Phone</li>
+            </ul>
+
+            <h1>Mobile Manufactures</h1>
+            <ul>
+                <li>Samsung</li>
+                <li>HTC</li>
+                <li>Micromax</li>
+                <li>Apple</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MobileOs
