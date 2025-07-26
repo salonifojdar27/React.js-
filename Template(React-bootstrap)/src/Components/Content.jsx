56355832d83ab2edb2275d8ad6c8f29720affddc
salonifojdar27 <salonifojdar27@gmail.com>
2025-07-26 12:29:48 +0530

    Update Content.jsx

--- Template(React-bootstrap)/src/Components/Content.jsx
+++ Template(React-bootstrap)/src/Components/Content.jsx
@@ -1,6 +1,7 @@
 
 import { Button } from "react-bootstrap";
 import { FaFacebookF, FaTwitter, FaInstagram, FaGithub } from "react-icons/fa";
+import Image from "./Image";
 
 const Content = () => {
     return (
@@ -24,10 +25,7 @@ const Content = () => {
                 </div>
 
                 {/* Right side - Image */}
-                <div className="col-md-6 text-center mt-5 mt-md-0">
-                    <img src="https://t3.ftcdn.net/jpg/06/41/26/64/360_F_641266494_Qy6PJV3b9ZvuFPg7f5vGEFMMsynp4NWZ.jpg" alt="John Doe" className="img-fluid hero-img"
-                    />
-                </div>
+                 <Image />
             </div>
         </div>
     )
